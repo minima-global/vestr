2c3e3bee1ad8635b47860cb3895f525e1f8cb01d
Elias <eliasnemr@gmail.com>
2023-02-15 19:30:09 +0200

    Added tabs, created a hook for tabs, differentiate pages

--- src/components/VestCreate/index.tsx
+++ src/components/VestCreate/index.tsx
@@ -12,10 +12,14 @@ import * as RPC from "../../minima/libs/RPC";
 import styles from "./VestCreate.module.css";
 
 import { isDate } from "date-fns";
+import { TabButton, Tabs } from "../MiCustom/MiTabs";
+import useTabs from "../../hooks/useTabs";
 
 const VestCreate = () => {
   // create wallet state
   const [wallet, setWallet] = useState<MinimaToken[]>([]);
+
+  const { tabs, setTabOpen, toggleTab, tabStyles } = useTabs();
 
   events.onNewBalance(() => {
     RPC.getMinimaBalance()
@@ -86,118 +90,136 @@ const VestCreate = () => {
     enableReinitialize: !!wallet,
   });
   return (
-    <Stack textAlign="center">
+    <Stack textAlign="center" spacing={1}>
       <h6 className={styles["form-header"]}>Lock Minima</h6>
-      <form onSubmit={formik.handleSubmit}>
-        <Stack spacing={5}>
-          <Stack spacing={1} alignItems="center">
-            {formik.status ? (
-              <div className={styles["form-error"]}>{formik.status}</div>
-            ) : null}
-            {formik.values.token ? (
-              <MiSelect
-                id="token"
-                name="token"
-                value={formik.values.token}
+      <Tabs>
+        <TabButton
+          onClick={() => toggleTab(0)}
+          className={tabs === 0 ? tabStyles["tab-open"] : undefined}
+        >
+          Create
+        </TabButton>
+        <TabButton
+          onClick={() => toggleTab(1)}
+          className={tabs === 1 ? tabStyles["tab-open"] : undefined}
+        >
+          Track
+        </TabButton>
+      </Tabs>
+
+      {tabs === 0 && (
+        <form onSubmit={formik.handleSubmit}>
+          <Stack spacing={5}>
+            <Stack spacing={1} alignItems="center">
+              {formik.status ? (
+                <div className={styles["form-error"]}>{formik.status}</div>
+              ) : null}
+              {formik.values.token ? (
+                <MiSelect
+                  id="token"
+                  name="token"
+                  value={formik.values.token}
+                  onChange={formik.handleChange}
+                  fullWidth={true}
+                  error={
+                    formik.touched.token && Boolean(formik.errors.token)
+                      ? true
+                      : false
+                  }
+                  tokens={wallet}
+                  setFieldValue={formik.setFieldValue}
+                  resetForm={formik.resetForm}
+                />
+              ) : null}
+              <TextField
+                fullWidth
+                id="address"
+                name="address"
+                placeholder="address"
+                value={formik.values.address}
                 onChange={formik.handleChange}
-                fullWidth={true}
-                error={
-                  formik.touched.token && Boolean(formik.errors.token)
-                    ? true
-                    : false
-                }
-                tokens={wallet}
-                setFieldValue={formik.setFieldValue}
-                resetForm={formik.resetForm}
+                onBlur={formik.handleBlur}
                 disabled={formik.isSubmitting}
               />
-            ) : null}
-            <TextField
+              <TextField
+                type="number"
+                fullWidth
+                id="amount"
+                name="amount"
+                placeholder="amount"
+                value={formik.values.amount}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                disabled={formik.isSubmitting}
+              />
+              <TextField
+                fullWidth
+                id="root"
+                name="root"
+                placeholder="root key"
+                value={formik.values.root}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                disabled={formik.isSubmitting}
+              />
+              <Select
+                id="cliff"
+                name="cliff"
+                value={formik.values.cliff}
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                disabled={formik.isSubmitting}
+              >
+                <option defaultValue={0} disabled value={0}>
+                  Set a cliff period
+                </option>
+                {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((i) => (
+                  <option key={i} value={i}>
+                    {i} month
+                  </option>
+                ))}
+              </Select>
+              <DateTimePicker
+                disablePast={true}
+                value={formik.values.endContract}
+                onChange={(value) => {
+                  formik.setFieldValue("endContract", value, true);
+                }}
+                renderInput={(params: any) => {
+                  return (
+                    <TextField
+                      disabled={formik.isSubmitting}
+                      fullWidth
+                      InputProps={{
+                        readOnly: true,
+                      }}
+                      error={Boolean(formik.errors.endContract)}
+                      helperText={formik.dirty && formik.errors.endContract}
+                      id="datetime"
+                      name="datetime"
+                      onBlur={formik.handleBlur}
+                      {...params}
+                    />
+                  );
+                }}
+              />
+            </Stack>
+            <Button
+              type="submit"
+              disableElevation
               fullWidth
-              id="address"
-              name="address"
-              placeholder="address"
-              value={formik.values.address}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              disabled={formik.isSubmitting}
-            />
-            <TextField
-              type="number"
-              fullWidth
-              id="amount"
-              name="amount"
-              placeholder="amount"
-              value={formik.values.amount}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              disabled={formik.isSubmitting}
-            />
-            <TextField
-              fullWidth
-              id="root"
-              name="root"
-              placeholder="root key"
-              value={formik.values.root}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              disabled={formik.isSubmitting}
-            />
-            <Select
-              id="cliff"
-              name="cliff"
-              value={formik.values.cliff}
-              onBlur={formik.handleBlur}
-              onChange={formik.handleChange}
+              color="inherit"
+              variant="contained"
               disabled={formik.isSubmitting}
             >
-              <option defaultValue={0} disabled value={0}>
-                Set a cliff period
-              </option>
-              {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((i) => (
-                <option key={i} value={i}>
-                  {i} month
-                </option>
-              ))}
-            </Select>
-            <DateTimePicker
-              disablePast={true}
-              value={formik.values.endContract}
-              onChange={(value) => {
-                formik.setFieldValue("endContract", value, true);
-              }}
-              renderInput={(params: any) => {
-                return (
-                  <TextField
-                    disabled={formik.isSubmitting}
-                    fullWidth
-                    InputProps={{
-                      readOnly: true,
-                    }}
-                    error={Boolean(formik.errors.endContract)}
-                    helperText={formik.dirty && formik.errors.endContract}
-                    id="datetime"
-                    name="datetime"
-                    onBlur={formik.handleBlur}
-                    {...params}
-                  />
-                );
-              }}
-            />
+              {!formik.isSubmitting && "Lock"}
+              {formik.isSubmitting && <CircularProgress size={16} />}
+            </Button>
           </Stack>
-          <Button
-            type="submit"
-            disableElevation
-            fullWidth
-            color="inherit"
-            variant="contained"
-            disabled={formik.isSubmitting}
-          >
-            {!formik.isSubmitting && "Lock"}
-            {formik.isSubmitting && <CircularProgress size={16} />}
-          </Button>
-        </Stack>
-      </form>
+        </form>
+      )}
+
+      {tabs === 1 && <div>Track your coins</div>}
     </Stack>
   );
 };
