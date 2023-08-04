bf23822d65624d3f704d6239703c73eff6b3b3c2
Elias <eliasnemr@gmail.com>
2023-08-04 18:43:06 +0300

    Hide footer on transaction creation :apple:

--- src/pages/creator/index.tsx
+++ src/pages/creator/index.tsx
@@ -4,6 +4,7 @@ import { Outlet, useLocation, matchPath, useNavigate } from "react-router-dom";
 import { CSSTransition } from "react-transition-group";
 import Contracts from "../../components/contracts";
 import { appContext } from "../../AppContext";
+import FadeIn from "../../components/UI/Animations/FadeIn";
 const Creator = () => {
   const navigate = useNavigate();
   const location = useLocation();
@@ -29,82 +30,63 @@ const Creator = () => {
 
   return (
     <>
-      <CSSTransition
-        in={
-          Boolean(createPath) ||
-          Boolean(reviewPath) ||
-          Boolean(contractDetailPath) ||
-          Boolean(calculatePath)
-        }
-        unmountOnExit
-        timeout={200}
-        classNames={{
-          enter: styles.backdropEnter,
-          enterDone: styles.backdropEnterActive,
-          exit: styles.backdropExit,
-          exitActive: styles.backdropExitActive,
-        }}
-      >
-        <Outlet />
-      </CSSTransition>
-      <CSSTransition
-        in={
-          !Boolean(createPath) &&
-          !Boolean(reviewPath) &&
-          !Boolean(contractDetailPath) &&
-          !Boolean(calculatePath)
-        }
-        unmountOnExit
-        timeout={200}
-        classNames={{
-          enter: styles.backdropEnter,
-          enterDone: styles.backdropEnterActive,
-          exit: styles.backdropExit,
-          exitActive: styles.backdropExitActive,
-        }}
-      >
-        <section className={styles["grid"]}>
-          <section>
-            <button onClick={() => navigate("calculate")} type="button">
-              <img alt="calculate-icon" src="./assets/calculate.svg" />
-              Calculate a contract
-            </button>
-            <button
-              type="button"
-              onClick={() => navigate("create", { state: { tokenid: "0x00" } })}
-            >
-              <img alt="add-icon" src="./assets/add.svg" />
-              Create a contract
-            </button>
-          </section>
-          <section>
-            <section>
-              <h6>My contracts</h6>
-              <input
-                type="search"
-                placeholder="Search contract"
-                onChange={(e: any) => setFilteredText(e.target.value)}
-              />
-            </section>
-            <section>
-              {!contracts.size && !filterText.length && (
-                <div>
-                  <p className={styles["no-contracts"]}>
-                    You currently have no vesting contracts
-                  </p>
-                </div>
-              )}
+      {(Boolean(createPath) ||
+        Boolean(reviewPath) ||
+        Boolean(contractDetailPath) ||
+        Boolean(calculatePath)) && (
+        <FadeIn delay={0}>
+          <Outlet />
+        </FadeIn>
+      )}
 
-              {!!contracts.size && (
-                <Contracts
-                  filterText={filterText}
-                  coins={Array.from(contracts.values())}
+      {!Boolean(createPath) &&
+        !Boolean(reviewPath) &&
+        !Boolean(contractDetailPath) &&
+        !Boolean(calculatePath) && (
+          <section className={styles["grid"]}>
+            <section>
+              <button onClick={() => navigate("calculate")} type="button">
+                <img alt="calculate-icon" src="./assets/calculate.svg" />
+                Calculate a contract
+              </button>
+              <button
+                type="button"
+                onClick={() =>
+                  navigate("create", { state: { tokenid: "0x00" } })
+                }
+              >
+                <img alt="add-icon" src="./assets/add.svg" />
+                Create a contract
+              </button>
+            </section>
+            <section>
+              <section>
+                <h6>My contracts</h6>
+                <input
+                  type="search"
+                  placeholder="Search contract"
+                  onChange={(e: any) => setFilteredText(e.target.value)}
                 />
-              )}
+              </section>
+              <section>
+                {!contracts.size && !filterText.length && (
+                  <div>
+                    <p className={styles["no-contracts"]}>
+                      You currently have no vesting contracts
+                    </p>
+                  </div>
+                )}
+
+                {!!contracts.size && (
+                  <Contracts
+                    filterText={filterText}
+                    coins={Array.from(contracts.values())}
+                  />
+                )}
+              </section>
             </section>
           </section>
-        </section>
-      </CSSTransition>
+        )}
     </>
   );
 };
